128a2450126364f208ca267111c7722b8221b4ee
MostafaHamdy3 <mostafa44hamdy@gmail.com>
2021-12-19 22:31:42 +0200

    for

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
-
+        int x = in.nextInt();
         String input ;
         while (!getOut){
             System.out.println("are you the admin ? (Y/N)");
